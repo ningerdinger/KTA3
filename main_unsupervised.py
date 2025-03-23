a0bf3e57ee878c4956faa19b6f6c969589482f38
Jorrit Adema <jorritadema@hotmail.com>
2025-03-23 21:10:30 +0100

    Added Calisnki

--- main_unsupervised.py
+++ main_unsupervised.py
@@ -1,7 +1,22 @@
 import pandas as pd
 from sklearn.cluster import KMeans
+from sklearn.metrics import calinski_harabasz_score
 import os
 import shutil
+
+def find_best_number_of_clusters(results_csv, min_clusters=2, max_clusters=10):
+    embeddings_df = pd.read_csv(results_csv)
+    embeddings = embeddings_df.T.values
+    best_score = -1
+    best_n_clusters = min_clusters
+    for n_clusters in range(min_clusters, max_clusters + 1):
+        kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(embeddings)
+        labels = kmeans.labels_
+        score = calinski_harabasz_score(embeddings, labels)
+        if score > best_score:
+            best_score = score
+            best_n_clusters = n_clusters
+    return best_n_clusters
 
 def separate_images_by_clusters(results_csv, faces_folder, output_base_folder, n_clusters=2):
     embeddings_df = pd.read_csv(results_csv)
@@ -23,4 +38,5 @@ RESULTS_CSV = 'D:\\KTAI\\assignments\\3\\results\\\\first_result.csv'
 FACES_FOLDER = 'D:\\KTAI\\assignments\\3\\face_folder\\'
 OUTPUT_BASE_FOLDER = 'clusters'
 
-separate_images_by_clusters(RESULTS_CSV, FACES_FOLDER, OUTPUT_BASE_FOLDER, n_clusters=8)
+best_clusers = find_best_number_of_clusters('results/first_result.csv')
+separate_images_by_clusters(RESULTS_CSV, FACES_FOLDER, OUTPUT_BASE_FOLDER, n_clusters=best_clusers)
