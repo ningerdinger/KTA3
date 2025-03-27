10e3b3648f7eb2e1be25046af47a8e60f78a6b3c
Sander Brouwer <sander@sbrouwer.nl>
2025-03-27 14:25:03 +0100

    opnieuw

--- main_embed.py
+++ main_embed.py
@@ -6,7 +6,7 @@ from embed import embed_face_net
 
 
 FACES_FOLDER_TRAINING = 'D:\KTAI\\assignments\\3\\repo\\face_folder\\'
-OUTPUT_FOLDER_RESULTS = 'D:\\KTAI\\assignments\\3\\repo\\results'
+OUTPUT_FOLDER_RESULTS = 'D:\\KTAI\\assignments\\3\\repo\\results\\'
 RESULTS_NAME = 'second_result.csv'
 directory = Path(FACES_FOLDER_TRAINING)
 
